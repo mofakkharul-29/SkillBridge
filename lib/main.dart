f7d98784dbd563ea2bb4ca6e1cd08dacc84857f6
mofakkharul-29 <mofakkharulislam29@gmail.com>
2026-05-31 03:44:08 +0600

    added

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:skill_bridge/core/utils/app_scale.dart';
-import 'package:skill_bridge/feature/splash/splash_screen.dart';
+import 'package:skill_bridge/feature/splash/presentation/splash_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();

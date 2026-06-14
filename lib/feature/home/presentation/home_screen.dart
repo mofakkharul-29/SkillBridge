ed16e5390b2443649dd3cc8a81956935d2b1b4cc
mofakkharul-29 <mofakkharulislam29@gmail.com>
2026-06-14 08:31:13 +0600

    added

--- lib/feature/home/presentation/home_screen.dart
+++ lib/feature/home/presentation/home_screen.dart
@@ -5,6 +5,6 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text('Home screen')));
+    return Scaffold(body: Center(child: Text('Home screen work here')));
   }
 }
